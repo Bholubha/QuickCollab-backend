678f93f402832f476b9a3bfeeb7249c111755371
Bhautik Vala <bhautiksinhvala333@gmail.com>
2024-07-22 11:07:52 +0530

    original

--- index.js
+++ index.js
@@ -1,60 +1,41 @@
 const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cluster = require('cluster');
-const os = require('os');
-
+const app = express();
 const PORT = 5000;
-const numCPUs = os.cpus().length;
 
-if (cluster.isMaster) {
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
   });
-} else {
-  const app = express();
-  
-  // Middleware for CORS
-  const cors = require('cors');
-  app.use(cors());
+});
 
-  const server = http.Server(app);
-  const io = socketIO(server, {
-    cors: {
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+const socketIO = require('socket.io')(http, {
+  cors: {
       origin: "*"
-    }
+  }
+});
+
+
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+
+  socket.on("sendCollab",(ele)=>{
+ 
+    if(ele != undefined && ele != null) socketIO.emit("getCollab",ele);
+  })
+  
+  socket.on('disconnect', () => {
+    console.log('🔥: A user disconnected');
   });
-
-
-  // Define a simple API endpoint
-  app.get('/api', (req, res) => {
-    res.json({
-      message: 'Hello world',
-    });
-  });
-
-  // Handle Socket.IO connections
-  io.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-
-    socket.on('sendCollab', (ele) => {
-      if (ele != undefined && ele != null) {
-        io.emit('getCollab', ele);
-      }
-    });
-
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-    });
-  });
-
-  // Start the server
-  server.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
-}
+});
